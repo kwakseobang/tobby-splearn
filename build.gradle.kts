64b350bb4bf0845558a39c65e31c4bdfcbd5e580
kwakseobang <iii6602@naver.com>
2025-06-19 22:32:58 +0900

    Setting: 스프링을 이용한 null 안정성 확

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.4.6"
     id("io.spring.dependency-management") version "1.1.7"
+    id("com.github.spotbugs") version "6.2.0"
 }
 
 group = "org.kwakmunsu"
@@ -26,11 +27,15 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    compileOnly("org.projectlombok:lombok")
+
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
-    annotationProcessor("org.projectlombok:lombok")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
---
+++ src/main/java/org/kwakmunsu/splearn/domain/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package org.kwakmunsu.splearn.domain;
+
+import org.springframework.lang.NonNullApi;
