a6f4be0c5bd82be5f017f46c32d9e4f1df02f520
kwakseobang <iii6602@naver.com>
2025-06-21 11:31:13 +0900

    Fix: value 에약어여서 address로 변경

--- src/main/java/org/kwakmunsu/splearn/domain/Email.java
+++ src/main/java/org/kwakmunsu/splearn/domain/Email.java
@@ -1,15 +1,17 @@
 package org.kwakmunsu.splearn.domain;
 
+import jakarta.persistence.Embeddable;
 import java.util.regex.Pattern;
 
-public record Email(String value) {
+@Embeddable
+public record Email(String address) {
 
     private static final Pattern EMAIL_PATTERN =
             Pattern.compile("^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,7}$");
 
     public Email {
-        if (!EMAIL_PATTERN.matcher(value).matches()) {
-            throw new IllegalArgumentException("이메일 형식이 옳바르지 않습니다." + value);
+        if (!EMAIL_PATTERN.matcher(address).matches()) {
+            throw new IllegalArgumentException("이메일 형식이 옳바르지 않습니다." + address);
         }
     }
 
