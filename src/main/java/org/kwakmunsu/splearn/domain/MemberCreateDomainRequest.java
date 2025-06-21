5813bfee93071f8573410b0e5aa38a026fc2e546
kwakseobang <iii6602@naver.com>
2025-06-21 11:30:31 +0900

    Refactor: dto 이름 변경

--- src/main/java/org/kwakmunsu/splearn/domain/MemberCreateDomainRequest.java
+++
@@ -1,5 +0,0 @@
-package org.kwakmunsu.splearn.domain;
-
-public record MemberCreateDomainRequest(String email, String nickname, String password) {
-
-}
---
+++ src/main/java/org/kwakmunsu/splearn/domain/MemberRegisterRequest.java
@@ -0,0 +1,5 @@
+package org.kwakmunsu.splearn.domain;
+
+public record MemberRegisterRequest(String email, String nickname, String password) {
+
+}
