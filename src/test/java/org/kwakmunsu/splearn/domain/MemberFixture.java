18088f1f81dbead1c5088dd133deadb976576205
kwakseobang <iii6602@naver.com>
2025-06-22 12:07:56 +0900

    Test: 멤버 회원 서비스 테스

--- src/test/java/org/kwakmunsu/splearn/domain/MemberFixture.java
+++ src/test/java/org/kwakmunsu/splearn/domain/MemberFixture.java
@@ -1,5 +1,7 @@
 package org.kwakmunsu.splearn.domain;
-
+/*
+* 테스트 코드에서 반복적으로 사용되는 도메인의 관련 데이터를 모아둔 클래스
+* */
 public class MemberFixture {
 
     public static MemberRegisterRequest createMemberRegisterRequest(String email) {
